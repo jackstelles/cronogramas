b87978b46078a739dcab68c67a4f1bb60782cae1
Jaquellyne Teles <jackstelles@gmail.com>
2023-05-31 11:33:17 -0300

    first model

---
+++ src/databases/models/curso.ts
@@ -0,0 +1,24 @@
+import { Entity, PrimaryColumn, Column } from "typeorm"
+import { v4 as uuid } from "uuid"
+
+@Entity("curso")
+export default class curso {
+    @PrimaryColumn()
+    id_curso: string
+
+    @Column({nullable: true})
+    descricao_curso: string
+
+    @Column({nullable: true})
+    carga_horaria_curso: number
+
+    @Column({nullable: true})
+    modalidade: string
+
+    @Column({nullable: true})
+    eixo: string
+
+    constructor () {
+        this.id_curso = uuid()
+    }
+}
