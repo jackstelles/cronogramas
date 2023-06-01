dc46f74471405ab02bc0eb0b5a4529350a091749
Jaquellyne Teles <jackstelles@gmail.com>
2023-06-01 11:45:19 -0300

    classes service

--- src/services/serviceTurma.ts
+++ src/services/serviceTurma.ts
@@ -1,14 +1,88 @@
 import Turma from "../databases/models/turma"
 import { AppDataSource } from "../databases/connections/data-source"
+import { IntegerType } from "typeorm"
 
 const cursor = AppDataSource.getRepository(Turma)
 
-export class CreateCursoService {}
+type newTurmaRequest = {
+    fk_curso: string
+    data_inicio: Date
+    data_fim: Date
+    horas_aula_dia: number
+  }
+  
+  type findOneTurmaRequest = {
+    id_turma: string
+  }
 
-export class ReadAllCursoService {}
+export class CreateTurmaService {
+    async execute({
+        fk_curso,
+        data_inicio,
+        data_fim,
+        horas_aula_dia,
+      }: newTurmaRequest): Promise<Turma | Error> {
+        // Se já existir um curso com a mesma descrição informada pelo usuário
+        // o sistema retornará uma mensagem de erro
+        if (await cursor.findOne({ where: { fk_curso } })) {
+          return new Error("Curso já cadastrado!")
+        }
+    
+        // Cria um objeto (APP) para ser salvo como registro (BD)
+        const turma = cursor.create({
+          fk_curso, 
+          data_inicio, 
+          data_fim, 
+          horas_aula_dia, 
+        })
+    
+        // Faz um INSERT lá na tabela "curso"
+        // com os dados informados pelo usuário
+        await cursor.save(turma)
+    
+        // Devolve pro frontend o objeto criado da classe "Curso"
+        return turma
+      }
+}
 
-export class ReadOneCursoService {}
+export class ReadAllTurmaService {
+  async execute() {
+    // Executa a consulta "SELECT * FROM curso" no BD
+    // Armazena todos os registros do Result Set na variável "cursos"
+    // Neste caso, esta variável é uma lista de cursos
+    const turmas = await cursor.find()
+    return turmas
+  }
+}
 
-export class UpdateCursoService {}
+export class ReadOneTurmaService {
+  // Recebe o ID do curso como parâmetro da Requisição do usuário
+  async execute({ id_turma }: findOneTurmaRequest) {
+    // Vê se o curso existe na tabela no BD - SELECT * FROM curso WHERE id_curso = ??
+    const turma = await cursor.findOne({ where: { id_turma } })
+    // Se o curso não for encontrado no Result Set retorna um erro para o usuário
+    if (!turma) {
+      return new Error("Turma não encontrada!")
+    }
+    // Se o curso for encontrado retorna para o usuário o curso
+    return turma
+  }
+}
 
-export class DeleteCursoService {}
+export class UpdateTurmaService {}
+
+export class DeleteTurmaService {
+  // Recebe o ID do curso como parâmetro da Requisição do usuário
+  async execute({ id_turma }: findOneTurmaRequest) {
+    // Vê se o curso existe na tabela no BD - SELECT * FROM curso WHERE id_curso = ??
+    const turma = await cursor.findOne({ where: { id_turma } })
+    // Se o curso não for encontrado no Result Set retorna um erro para o usuário
+    if (!turma) {
+      return new Error("Turma não encontrado!")
+    }
+    // Se o curso for encontrado, deleta do BD - DELETE FROM curso WHERE id_curso = ??
+    await cursor.delete(turma.id_turma)
+    // Retorna para o usuário o curso que foi deletado
+    return turma
+  }
+}
--- src/services/serviceUnidade.ts
+++ src/services/serviceUnidade.ts
@@ -1,14 +1,83 @@
-import Curso from "../databases/models/curso"
+import Unidade from "../databases/models/unidade"
 import { AppDataSource } from "../databases/connections/data-source"
 
-const cursor = AppDataSource.getRepository(Curso)
+const cursor = AppDataSource.getRepository(Unidade)
 
-export class CreateCursoService {}
+type newUnidadeRequest = {
+    fk_curso: string
+    descricao_unidade: string
+    carga_horaria_unidade: number
+    ordem: number
+  }
 
-export class ReadAllCursoService {}
+  type findOneUnidadeRequest = {
+    id_unidade: string
+  }
+export class CreateUnidadeService {
+    async execute({
+        fk_curso,
+        descricao_unidade,
+        carga_horaria_unidade,
+        ordem,
+      }: newUnidadeRequest): Promise<Unidade | Error> {
+        // Se já existir um curso com a mesma descrição informada pelo usuário
+        // o sistema retornará uma mensagem de erro
+        if (await cursor.findOne({ where: { fk_curso } })) {
+          return new Error("Curso já cadastrado!")
+        }
+    
+        // Cria um objeto (APP) para ser salvo como registro (BD)
+        const unidade = cursor.create({
+            fk_curso,
+            descricao_unidade,
+            carga_horaria_unidade,
+            ordem,
+        })
+    
+        // Faz um INSERT lá na tabela "curso"
+        // com os dados informados pelo usuário
+        await cursor.save(unidade)
+    
+        // Devolve pro frontend o objeto criado da classe "Curso"
+        return unidade
+      }
+}
 
-export class ReadOneCursoService {}
+export class ReadAllUnidadeService {
+    async execute() {
+        const unidades = await cursor.find()
+        return unidades
+      }
+}
 
-export class UpdateCursoService {}
+export class ReadOneUnidadeService {
+    
+  async execute({ id_unidade }: findOneUnidadeRequest) {
+    
+    const unidade = await cursor.findOne({ where: { id_unidade } })
+    
+    if (!unidade) {
+      return new Error("Unidade não encontrado!")
+    }
+    
+    return unidade
+  }
+}
 
-export class DeleteCursoService {}
+export class UpdateUnidadeService {}
+
+export class DeleteUnidadeService {
+    // Recebe o ID do curso como parâmetro da Requisição do usuário
+  async execute({ id_unidade }: findOneUnidadeRequest) {
+    // Vê se o curso existe na tabela no BD - SELECT * FROM curso WHERE id_curso = ??
+    const unidade = await cursor.findOne({ where: { id_unidade } })
+    // Se o curso não for encontrado no Result Set retorna um erro para o usuário
+    if (!unidade) {
+      return new Error("Unidade não encontrado!")
+    }
+    // Se o curso for encontrado, deleta do BD - DELETE FROM curso WHERE id_curso = ??
+    await cursor.delete(unidade.id_unidade)
+    // Retorna para o usuário o curso que foi deletado
+    return unidade
+  }
+}
